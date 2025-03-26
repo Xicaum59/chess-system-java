8507e6e45c2e0a8ef0091e7a64b9cd68db13fe3b
Xico Utrabo <xico.utrabo@gmail.com>
2025-03-26 18:38:40 -0300

    Possible Moves of a Piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -5,6 +5,7 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+	
 	public King(Board board, Color color) {
 		super(board, color);
 		
@@ -16,5 +17,11 @@ public class King extends ChessPiece {
 
 	{
 }
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 
 }
